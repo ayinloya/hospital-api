b43fc1fa880fe5214aa2540bf14a770a440e16bb
Barnabas A Nsoh <barnabas.ayinloya@meltwater.org>
2014-11-14 11:22:11 +0000

    added root to routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :medical_histories
-
+  root 'medical_histories#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
